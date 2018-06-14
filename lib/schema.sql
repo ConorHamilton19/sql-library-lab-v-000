67c8561245859fa77b73c6d7c2a72ff7670c8e04
Conor Hamilton <conhamil19@gmail.com>
2018-06-14 06:22:52 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,1 +1,4 @@
 CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub_genre_id INTEGER)
+CREATE TABLE sub_genre (id INTEGER PRIMARY KEY, name TEXT)
+CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER)
+CREATE TABLE character (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub_genre_id INTEGER)
