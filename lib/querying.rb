00ab6e439602aad8f332e6c920f3b87079e72bd2
Conor Hamilton <conhamil19@gmail.com>
2018-06-14 07:05:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON series.id = characters.series_id
-  GROUP BY series.title 
+   
   ORDER BY COUNT(characters.species = 'human') DESC LIMIT 1"
 end
 
