9928c8b432eee7d985008914f7eba00c2d60324d
Conor Hamilton <conhamil19@gmail.com>
2018-06-14 06:43:01 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year FROM books
-  JOIN series BY"
+  JOIN series ON series.id = books.series_id"
+  
 end
 
 def select_name_and_motto_of_char_with_longest_motto
